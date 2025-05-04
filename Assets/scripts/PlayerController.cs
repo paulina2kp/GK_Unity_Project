47809a892e12d0f2773ec5714ed08ea2ac307d01
paulina2kp <115478320+paulina2kp@users.noreply.github.com>
2025-05-04 23:08:42 +0200

    Adding walking animation and function

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    public Rigidbody my_Rigidbody;
+    public Animator my_Animator;
+    public SpriteRenderer my_SpriteRenderer;
+    public Transform sprite_Transform;
+    public float move_Speed;
+
+    private Vector2 move_Input;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move_Input.x = Input.GetAxis("Horizontal");
+        move_Input.y = Input.GetAxis("Vertical");
+
+        my_Rigidbody.linearVelocity = new Vector3(move_Input.x * move_Speed, my_Rigidbody.linearVelocity.y, move_Input.y * move_Speed);
+
+        if(move_Input.x == 0 && move_Input.y == 0)
+        {
+            my_Animator.SetBool("isMoving", false);
+        }
+        else
+        {
+            my_Animator.SetBool("isMoving", true);
+        }
+
+        if (!my_SpriteRenderer.flipX && move_Input.x < 0)
+        {       
+            sprite_Transform.localScale = new Vector3(-1, 1, 1);
+            my_SpriteRenderer.flipX = true;
+        }
+        else if (my_SpriteRenderer.flipX && move_Input.x > 0)
+        {
+            sprite_Transform.localScale = new Vector3(1, 1, 1);
+            my_SpriteRenderer.flipX = false;
+        }
+
+    }
+
+}
